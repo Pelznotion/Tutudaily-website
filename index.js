61f30979d3182bdc05239ec6e010e1417349767b
Pelznotion <145939369+Pelznotion@users.noreply.github.com>
2024-05-05 13:26:32 -1100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ document.querySelector(".ham-img").addEventListener("click", showNav);
 function showNav() {
   document.querySelector(".nav__links").classList.toggle("show__links");
 }
+let allbtn = document.querySelectorAll("single-services");
+
+let hover = (e) => {
+  e.target.classList.add("hover");
+};
+let end = (e) => {
+  e.target.classList.remove("hover");
+};
+allbtn.forEach((singleServices) => {
+  singleServices.addEventListener("touchstart", hover);
+  singleServices.addEventListener("touchend", end);
+});
